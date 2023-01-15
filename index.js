fe92b144fb21063b69ea857fa05da06db6e8fc2e
KIMTAEKJU <wpaltm123@gmail.com>
2023-01-15 21:37:49 +0900

    feat: 로그인 기능 추가
    1. bcrypt, jsonwebtoken, cookie-parser 라이브러리 추가
    2. bcrypt: 암호화 라이브러리
    jsonwebtoken: 토큰 생성 라이브러리
    cookie-parser: 쿠키 저장 라이브러리
    3. 로그인 기능 추가

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const mongoose = require('mongoose');
 const { User } = require('./models/User');
 const bodyParser = require('body-parser');
 const config = require('./config/key');
+const cookieParser = require('cookie-parser');
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI,
 {
@@ -49,7 +51,42 @@ app.post('/register', (req, res) => {
         })
 
     });
+});
+
+app.post('/login', (req, res) => {
+
+    /**
+     * 요청된 이메일을 데이터베이스에서 있는지 찾는다
+     */
+    User.findOne({ email: req.body.email }, (err, user) => {
+      
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: '제공된 이메일에 해당하는 유저가 없습니다.'
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+
+            if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.'});
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                /**
+                 * 토큰을 저장한다
+                 * 1. 쿠키
+                 * 2. 로컬스토리지
+                 */
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id });
+            })
+        })
+    })
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,7 +32,55 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-})
+});
+
+userSchema.pre('save', function (next) {
+    
+    /**
+     * 비밀번호 암호화
+     */
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+
+        const user = this;
+        console.log('user: ', user);
+        if (user.isModified('password')) {
+
+            if (err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                // Store hash in your password DB.
+    
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        } else {
+            next();
+        }
+    });
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+};
+
+userSchema.methods.generateToken = function (cb) {
+    // jsonwebtoken을 이용하여 생성
+
+    const user = this;
+    const token = jwt.sign(user._id.toJSON(), 'secretToken');
+
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    })
+};
 
 const User = mongoose.model('User', userSchema);
 
