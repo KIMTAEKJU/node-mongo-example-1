2b1fde926aa8be55e9e9a16ce06ba50c442a9ab5
KIMTAEKJU <wpaltm123@gmail.com>
2023-01-15 20:07:51 +0900

    feat: 라이브러리 설치 및 mongoURI 분리
    1. nodemon 설치
    2. mongoURI 분기 처리

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const app = express();
 const port = 3000;
 
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
 // mongoose.set("strictQuery", false);
 
-mongoose.connect('mongodb+srv://xorwn:xorwn@practice-mongo.tvzfnba.mongodb.net/?retryWrites=true&w=majority',
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI,
 {
     /**
      * 안쓰면 에러남
@@ -22,8 +32,24 @@ mongoose.connect('mongodb+srv://xorwn:xorwn@practice-mongo.tvzfnba.mongodb.net/?
 .catch((e) => console.error('error: ', e));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World1');
 });
+
+app.post('/register', (req, res) => {
+    /**
+     * 회원 가입 할때 필요한 정보들을 client에서 가져오면
+     * 그것들을 데이터 베이스에 넣어준다
+     */
+
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+
+    });
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
